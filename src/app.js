ad6240ca58f6297f4160d0bc7295311788f9fe04
Adnan <dsa567hjj@gmail.com>
2022-04-08 17:19:00 +0530

    Add path

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-require("dotenv").config()
+const path = require("path")
+require('dotenv').config({ path: path.join(__dirname, '../.env.development') })
 const express = require("express")
 const mongoose = require("mongoose")
 const morgan = require("morgan")
