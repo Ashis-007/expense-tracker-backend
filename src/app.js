420e06c6089b7163d53bb10bba60ef0ffee3107e
Ashis-007 <ashisjc@gmail.com>
2022-04-09 12:48:48 +0530

    Format app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
-require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` })
-const express = require("express")
-const mongoose = require("mongoose")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
-const {MONGO_LINK} = require("./utils/config")
+require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const { MONGO_LINK } = require("./utils/config");
 
-mongoose.connect(MONGO_LINK,{UseNewUrlParser: true}, (err) => {
-  if(err) console.log(err)
-  console.log("CONNECTED TO DB")
-})
+mongoose.connect(MONGO_LINK, { UseNewUrlParser: true }, (err) => {
+  if (err) console.log(err);
+  console.log("CONNECTED TO DB");
+});
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Expense Tracker backend up and runnin")
-})
+app.get("/", (req, res) => {
+  res.send("Expense Tracker backend up and runnin");
+});
 
-console.log(process.env.NODE_ENV)
-console.log(process.env.MONGO_LINK)
+console.log(process.env.NODE_ENV);
+console.log(process.env.MONGO_LINK);
 
-module.exports = app
+module.exports = app;
+
